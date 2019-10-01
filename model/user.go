ae23f7fa5c7f20357ea778c98fec5db5c399813b
FloydWatson <floydbcseit@gmail.com>
2019-10-01 21:49:07 +1300

    created user model

---
+++ model/user.go
@@ -0,0 +1,10 @@
+package main
+
+type User struct {
+	UserID     int    `json:"userID"`
+	GivenName  string `json:"givenName"`
+	FamilyName string `json:"familyName"`
+	UserName   string `json:"userName"`
+	Password   string `json:"password"` //This will normally be encripted
+	SessionID  string `json:"sessionID"`
+}
