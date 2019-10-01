8ab41bc2269058f7fc66b94f12bee32d2dbb8921
FloydWatson <floydbcseit@gmail.com>
2019-10-01 21:57:33 +1300

    packages not right

---
+++ model/note.go
@@ -0,0 +1,9 @@
+package note
+
+type Note struct {
+	NoteID int    `json:"noteID"`
+	UserID int    `json:"userID"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	Date   string `json:"Date"` // may change to datetime or just parse it
+}
