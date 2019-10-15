05dd4c624ce8d8c42be2cbef2e673873b81bfa70
FloydWatson <floydbcseit@gmail.com>
2019-10-15 20:24:24 +1300

    final master layout

--- controller.go
+++ controller.go
@@ -47,59 +47,6 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// //Validate if the username already exists in the database  (username has to be unique)
-// //Return true if username exists
-// func userNameExists(username string) bool {
-// 	var name string
-// 	//Connect to db
-// 	db := connectDatabase()
-// 	defer db.Close()
-
-// 	//Prepare query to check if the username already exists
-// 	getUserName, err := db.Prepare("Select user_name FROM _user WHERE user_name = $1;")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	err = getUserName.QueryRow(username).Scan(&name)
-// 	//if error username does not exist in database
-// 	if err == sql.ErrNoRows {
-// 		return false
-// 	}
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-// 	//Username does exist in the database
-// 	return true
-// }
-
-// //===================Validate Password=============================
-// func validatePass(password string) bool {
-// 	var pass string
-// 	//Connect to db
-// 	db := connectDatabase()
-// 	defer db.Close()
-
-// 	//prepare statement to check for password
-// 	stmt, err := db.Prepare("SELECT password FROM _user WHERE password = $1;")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	err = stmt.QueryRow(password).Scan(&pass)
-// 	//if nothing is returned
-// 	if err == sql.ErrNoRows {
-// 		//password does not match
-// 		return false
-// 	}
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	//password matches
-// 	return true
-
-// }
-
 //====================ADD USER=====================================
 func addUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Entered addUser()") // For testing
@@ -110,28 +57,6 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	json.Unmarshal(reqBody, &newUser)
 
 	fmt.Fprintf(w, addUserSQL(newUser))
-
-	//Create connection to server
-	//Connect to db
-	// db := connectDatabase()
-	// defer db.Close()
-
-	// if !userNameExists(newUser.UserName) {
-	// 	//Prepare insert to stop SQL injections
-	// 	log.Println("Entered add user if statement")
-	// 	stmt, err := db.Prepare("INSERT INTO _user VALUES($1,$2,$3,$4,$5);")
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-
-	// 	_, err = stmt.Exec(newUser.UserName, newUser.Password, newUser.Email, newUser.GivenName, newUser.FamilyName)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Fprintf(w, "Added user")
-	// } else {
-	// 	fmt.Fprintf(w, "Username already exists")
-	// }
 
 }
 
@@ -157,23 +82,6 @@ func addNote(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Fprintf(w, addNoteSQL(newNote))
 
-		//Connect to db
-		// db := connectDatabase()
-		// defer db.Close()
-
-		// //Prepare insert to stop SQL injections
-		// stmt, err := db.Prepare("INSERT INTO _note (title, body, date_created, note_owner) VALUES($1,$2,$3,$4);")
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		// _, err = stmt.Exec(newNote.NoteTitle, newNote.NoteBody, newNote.CreatedDate, newNote.NoteOwner)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-
-		// fmt.Fprintf(w, "New Note Added")
-		// //User is not logged in
 	} else {
 		fmt.Fprintf(w, "You are not logged in!")
 	}
@@ -192,21 +100,7 @@ func listNotes(w http.ResponseWriter, r *http.Request) {
 		}
 		username := usernameCookie.Value
 		notes := listAllNotesSQL(username)
-		//var notes []Note
-		//Connect to db
-		// db := connectDatabase()
-		// defer db.Close()
-		// stmt, err := db.Prepare("SELECT _note.note_id, _note.note_owner, _note.title, _note.body, _note.date_created FROM _note LEFT OUTER JOIN _note_privileges ON (_note.note_id = _note_privileges.note_id) WHERE _note.note_owner = $1 OR _note_privileges.user_name = $1;")
-		// var note Note
-		// rows, err := stmt.Query(username)
-		// for rows.Next() {
-		// 	err = rows.Scan(&note.NoteID, &note.NoteOwner, &note.NoteTitle, &note.NoteBody, &note.CreatedDate)
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-		// 	notes = append(notes, note)
 
-		// }
 		fmt.Println(notes)
 		//just sending it straight to frontend for testing
 		w.Header().Set("Content-Type", "application/json")
@@ -221,24 +115,6 @@ func listNotes(w http.ResponseWriter, r *http.Request) {
 //============================================LIST ALL REGISTERED USERS=====================
 //This function creates a list of all registerd users
 func listAllUses(loggedInUser string) []string {
-	// var users []string
-	// var username string
-	// //Connect to db
-	// db := connectDatabase()
-	// defer db.Close()
-
-	// //Send query to the db
-	// rows, err := db.Query("SELECT user_name FROM _user;")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// for rows.Next() {
-	// 	err = rows.Scan(&username)
-	// 	//Make sure we dont add logged in user to recommended
-	// 	if loggedInUser != username {
-	// 		users = append(users, username)
-	// 	}
-	// }
 
 	return listAllUsersSQL(loggedInUser)
 }
@@ -279,28 +155,7 @@ func searchNotePartial(w http.ResponseWriter, r *http.Request) {
 		bodyText := mux.Vars(r)["id"]
 		username := getUserName(r)
 		notes := partialTextSearchSQL(bodyText, username)
-		//Connect to db
-		// db := connectDatabase()
-		// defer db.Close()
 
-		// bodyText += ":*" //for testing
-		// stmt, err := db.Prepare("SELECT _note.note_id, _note.note_owner, _note.title, _note.body, _note.date_created FROM _note LEFT OUTER JOIN _note_privileges ON (_note.note_id = _note_privileges.note_id) WHERE body ~ $2 AND _note.note_owner = $1 OR _note_privileges.user_name = $1;")
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// var note Note
-		// rows, err := stmt.Query(username, bodyText)
-		// if err != nil {
-		// 	log.Fatal(err)
-		// }
-		// for rows.Next() {
-		// 	err = rows.Scan(&note.NoteID, &note.NoteTitle, &note.NoteBody, &note.CreatedDate, &note.NoteOwner)
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-		// 	notes = append(notes, note)
-
-		// }
 		fmt.Println(notes)
 		//just sending it straight to frontend for testing
 		w.Header().Set("Content-Type", "application/json")
