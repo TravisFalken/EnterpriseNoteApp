61a1f6ca79462109bde2aeb99cb5317b136241c8
FloydWatson <floydbcseit@gmail.com>
2019-11-05 23:49:41 +1300

    fixed db test

--- client/database_test.go
+++ client/database_test.go
@@ -81,16 +81,8 @@ func TestDatabase(t *testing.T) {
 		// test delete all remaining user notes
 		assert.True(deleteAllUserNotesSQL("testUserName"), "should return deleted note")
 
-<<<<<<< HEAD
-		// all tests end here
-		assert.True(t, deleteAllUserNotesSQL("testUserName"), "should return deleted note. possibly have extra notes in table") // have not tested individual note deletion. need to set up du,mmy tables for that
-		assert.True(t, deleteSpecificUserSQL("testUserName"), "Should delete test user")
-
-		// test
-=======
 		// test delete test user
 		assert.True(deleteSpecificUserSQL("testUserName"), "Should delete test user")
->>>>>>> testing
 	}
 
 }
