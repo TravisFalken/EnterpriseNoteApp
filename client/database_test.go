c8067307c1f8ea4e39ed58fb2ab89544f80cd0cf
FloydWatson <floydbcseit@gmail.com>
2019-11-05 23:01:35 +1300

    updated to multiple assert layout

--- client/database_test.go
+++ client/database_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 func TestDatabase(t *testing.T) {
+	assert := assert.New(t)
+
 	var newUser User
 	newUser.UserName = "testUserName"
 	newUser.GivenName = "testGivenName"
@@ -31,56 +33,56 @@ func TestDatabase(t *testing.T) {
 	defer db.Close()
 
 	// Initial test that database is there and connection can be made
-	if assert.NotNil(t, db) {
+	if assert.NotNil(db) {
 
 		// Test Add user, This user is used in testing
-		assert.Equal(t, "Added user", addUserSQL(newUser), "Should return added user success message")
+		assert.Equal("Added user", addUserSQL(newUser), "Should return added user success message")
 		// tests utilizing user start here
 
 		// Test user name can be found on database
-		assert.True(t, userNameExists("testUserName"), "Username should exist")
+		assert.True(userNameExists("testUserName"), "Username should exist")
 
 		// test password can be found and validate it for Test user
-		assert.True(t, validatePass("password", "testUserName"), "User should exist")
+		assert.True(validatePass("password", "testUserName"), "User should exist")
 
 		// Test Add new note. this note will be used in testing
-		assert.Equal(t, "New Note Added", addNoteSQL(newNote1), "Should return success message")
-		assert.Equal(t, "New Note Added", addNoteSQL(newNote2), "Should return success message")
+		assert.Equal("New Note Added", addNoteSQL(newNote1), "Should return success message")
+		assert.Equal("New Note Added", addNoteSQL(newNote2), "Should return success message")
 		// tests utilizing note start here
 
 		// list of notes created for test functions. Testing list all notes
 		notes := listAllNotesSQL("testUserName")
-		assert.Equal(t, "testUserName", notes[0].NoteOwner, "note should exist with user name")
+		assert.Equal("testUserName", notes[0].NoteOwner, "note should exist with user name")
 
 		// list of all users created for testing. testing that users can be found on data base
 		users := listAllUsersSQL("n/a") // na is dummy name added to function
-		assert.NotEqual(t, "", users[0], "Should not be null, should have users")
+		assert.NotEqual("", users[0], "Should not be null, should have users")
 
 		// note returned from partial search of body. Test that note returned was correct
 		partialSearchedNoteBody := partialTextBodySearchSQL("Parti", "testUserName")
-		assert.Contains(t, partialSearchedNoteBody[0].NoteBody, "Parti", "note should exist")
+		assert.Contains(partialSearchedNoteBody[0].NoteBody, "Parti", "note should exist")
 
 		// note return from partial title search. Test returned note was correct
 		partialSearchedNoteOwnedTitle := partialSeachOwnedTitleSQL("usi", "testUserName")
-		assert.Contains(t, partialSearchedNoteOwnedTitle[0].NoteTitle, "usi", "note should exist")
+		assert.Contains(partialSearchedNoteOwnedTitle[0].NoteTitle, "usi", "note should exist")
 
 		// note return for priveleileges of note partial text search. test of part of note search
 		partialSearchedNotePartOfTitle := partialSearchPartOfTitleSQL("t", "Trav3")
-		assert.Contains(t, partialSearchedNotePartOfTitle[0].NoteTitle, "t", "note should exist")
+		assert.Contains(partialSearchedNotePartOfTitle[0].NoteTitle, "t", "note should exist")
 
 		// notes returned for owned notes. Test owned notes have correct owner
 		ownedNotes := getOwnedNotesSQL("testUserName")
-		assert.Equal(t, "testUserName", ownedNotes[0].NoteOwner, "Note should have owner of testUserName")
+		assert.Equal("testUserName", ownedNotes[0].NoteOwner, "Note should have owner of testUserName")
 
 		// all user and note tests end here
 		// Test Delete specific note
-		assert.True(t, deleteSpecificNoteSQL(strconv.Itoa(partialSearchedNoteBody[0].NoteID), "testUserName"), "should return deleted note")
+		assert.True(deleteSpecificNoteSQL(strconv.Itoa(partialSearchedNoteBody[0].NoteID), "testUserName"), "should return deleted note")
 
 		// test delete all remaining user notes
-		assert.True(t, deleteAllUserNotesSQL("testUserName"), "should return deleted note")
+		assert.True(deleteAllUserNotesSQL("testUserName"), "should return deleted note")
 
 		// test delete test user
-		assert.True(t, deleteSpecificUserSQL("testUserName"), "Should delete test user")
+		assert.True(deleteSpecificUserSQL("testUserName"), "Should delete test user")
 	}
 
 }
