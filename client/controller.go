e3f5ad858342f6d432a203ac979ea7273526d208
FloydWatson <floydbcseit@gmail.com>
2019-11-06 20:19:57 +1300

    checkout sync

--- client/controller.go
+++ client/controller.go
@@ -59,7 +59,7 @@ func createNote(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-//====================EDIT NOTE PAGE======================================
+//====================EDIT NOTE PAGE================================================
 func editNote(w http.ResponseWriter, r *http.Request) {
 	//Make sure user is still logged in
 	if userStillLoggedIn(r) {
